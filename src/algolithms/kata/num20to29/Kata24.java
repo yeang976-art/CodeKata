ee0c774b4457b9dc71a63de8cf9aefc40c535149
yeang976-art <yeang976@gmail.com>
2026-06-29 10:27:45 +0900

    2026.06.29 / 10:27

--- src/algolithms/kata/num20to29/Kata24.java
+++ src/algolithms/kata/num20to29/Kata24.java
@@ -6,12 +6,17 @@ public final class Kata24 {
     public String solution(String[] seoul) {
         int i = IntStream.range(0, seoul.length)
                 .filter(a -> seoul[a].equals("Kim"))
-                .findFirst().orElseThrow();
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("김서방은 없다"));
         return "김서방은 " + i + "에 있다";
     }
 
     static void main() {
-        String[] s = {"Park","Hwang","Kim"};
-        System.out.println(new Kata24().solution(s));
+        try{
+            String[] s = {"Park","Hwang","Lee"};
+            System.out.println(new Kata24().solution(s));
+        } catch (IllegalStateException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
