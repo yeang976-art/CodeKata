e9b9db4f29c61f1645e7a1632e6417887deaed1b
yeang976-art <yeang976@gmail.com>
2026-07-22 09:50:15 +0900

    2026.07.22 / 09:50

---
+++ src/algolithms/kata/num30to39/Kata31.java
@@ -0,0 +1,16 @@
+package algolithms.kata.num30to39;
+
+public final class Kata31 {
+    public String solution(int n) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            sb.append(i % 2 == 0 ? "박" : "수");
+        }
+        return sb.toString();
+        //return sb.chars().map(a -> sb.append(a % 2 == 0 ? "박" : "수")).;
+    }
+
+    static void main() {
+        System.out.println(new Kata31().solution(12));
+    }
+}
